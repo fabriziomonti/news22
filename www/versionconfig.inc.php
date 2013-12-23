e149acf308db2754a9c92f5e5e9885744fde1f07
fabrizio monti <fabrizio.monti@webappls.com>
2013-12-23 19:09:37 +0100

    aggiunto incolonnamento thread commenti/interventi

--- www/versionconfig.inc.php
+++ www/versionconfig.inc.php
@@ -3,8 +3,8 @@ if (!defined('__VERSIONCONFIG_VARS'))
 {
 	define('__VERSIONCONFIG_VARS',1);
 	
-	define('APPL_REL', 							'1.0.3');
-	define('APPL_REL_DATE', 					mktime(0,0,0, 12, 20, 2013));
+	define('APPL_REL', 							'1.0.4');
+	define('APPL_REL_DATE', 					mktime(0,0,0, 12, 23, 2013));
 
 	
 } //  if (!defined('__VERSIONCONFIG_VARS'))
