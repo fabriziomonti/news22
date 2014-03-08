bea80f1df00f5193acecf771ee505bfcc317eaf6
fabrizio monti <fabrizio.monti@webappls.com>
2014-03-08 01:34:18 +0100

    corretta gestione feed

--- www/versionconfig.inc.php
+++ www/versionconfig.inc.php
@@ -3,7 +3,7 @@ if (!defined('__VERSIONCONFIG_VARS'))
 {
 	define('__VERSIONCONFIG_VARS',1);
 	
-	define('APPL_REL', 							'1.0.6');
+	define('APPL_REL', 							'1.0.7');
 	define('APPL_REL_DATE', 					mktime(0,0,0, 3, 8, 2013));
 
 	
